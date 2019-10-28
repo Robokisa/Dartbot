727e191cee0db4c6472013873c2d18fdc457757e
jennapalojarvi <56635307+jennapalojarvi@users.noreply.github.com>
2019-10-28 14:54:32 +0200

    Update readservo.py

--- readservo.py
+++ readservo.py
@@ -7,31 +7,31 @@ kit = ServoKit(channels=16)
 
 def readservo():
 
-    if kit.servo[14].angle <95 and >85:
+    if kit.servo[14].angle <95 and kit.servo[14].angle >85:
         eteen()
 
-    elif kit.servo[14].angle <180 and >160:
+    elif kit.servo[14].angle <180 and kit.servo[14].angle >160:
         carTurnO()
 
-    elif kit.servo[14].angle <20 and >0:
+    elif kit.servo[14].angle <20 and kit.servo[14].angle >0:
         carTurnV()
 
-    elif kit.servo[14].angle <40 and >20:
+    elif kit.servo[14].angle <40 and kit.servo[14].angle >20:
         carTurnV60()
 
-    elif kit.servo[14].angle <60 and >40:
+    elif kit.servo[14].angle <60 and kit.servo[14].angle >40:
         carTurnV40()
 
-    elif kit.servo[14].angle <85 and >60:
+    elif kit.servo[14].angle <85 and kit.servo[14].angle >60:
         carTurnV20()
 
-    elif kit.servo[14].angle <120 and >95:
+    elif kit.servo[14].angle <120 and kit.servo[14].angle >95:
         carTurnO20()
 
-    elif kit.servo[14].angle <140 and >120:
+    elif kit.servo[14].angle <140 and kit.servo[14].angle >120:
         carTurnO40()
 
-    elif kit.servo[14].angle <160 and >140:
+    elif kit.servo[14].angle <160 and kit.servo[14].angle >140:
         carTurnO60()
 
     sleep(1,5)
