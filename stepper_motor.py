d4814d91415a0415d0a7a54ec0fd50cc79431ba2
jennapalojarvi <56635307+jennapalojarvi@users.noreply.github.com>
2019-10-30 09:57:48 +0200

    Update stepper_motor.py

--- stepper_motor.py
+++ stepper_motor.py
@@ -11,9 +11,6 @@ GPIO.setwarnings(False)
 
 # Enable GPIO pins for  ENA and ENB for stepper
 
-enable_a = 6
-enable_b = 5
-
 # Enable pins for IN1-4 to control step sequence
 
 coil_A_1_pin = 15
@@ -23,8 +20,6 @@ coil_B_2_pin = 37
 
 # Set pin states
 
-GPIO.setup(enable_a, GPIO.OUT)
-GPIO.setup(enable_b, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
@@ -32,8 +27,6 @@ GPIO.setup(coil_B_2_pin, GPIO.OUT)
 
 # Set ENA and ENB to high to enable stepper
 
-GPIO.output(enable_a, True)
-GPIO.output(enable_b, True)
 
 # Function for step sequence
 
