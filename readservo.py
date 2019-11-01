f66928399a4efd371e39c84cc95b7258ba5be2b2
JannePernu <55883062+JannePernu@users.noreply.github.com>
2019-11-01 09:54:30 +0000

    Add files via upload

--- readservo.py
+++ readservo.py
@@ -1,65 +1,40 @@
 
-
+from adafruit_servokit import ServoKit
 import tyres as ty
-from time import sleep
 
-S1 = 5
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(S1, GPIO.OUT)
-pwm=GPIO.PWM(03, 50)
-
-
-def SetAngle(angle):
-
-	duty = angle / 18 + 2
-
-	GPIO.output(S1, True)
-
-	pwm.ChangeDutyCycle(duty)
-
-	sleep(1)
-
-	GPIO.output(S1, False)
-
-	pwm.ChangeDutyCycle(0)
-
-
+kit = ServoKit(channels=16)
 
 
 def readservo():
 
-    if angle <95 and angle >85:
+    if kit.servo[14].angle <95 and kit.servo[14].angle >85:
         eteen()
 
-    elif angle <180 and angle >160:
+    elif kit.servo[14].angle <180 and kit.servo[14].angle >160:
         carTurnO()
 
-    elif angle <20 and angle >0:
+    elif kit.servo[14].angle <20 and kit.servo[14].angle >0:
         carTurnV()
 
-    elif angle <40 and angle >20:
+    elif kit.servo[14].angle <40 and kit.servo[14].angle >20:
         carTurnV60()
 
-    elif angle <60 and angle >40:
+    elif kit.servo[14].angle <60 and kit.servo[14].angle >40:
         carTurnV40()
 
-    elif angle <85 and angle >60:
+    elif kit.servo[14].angle <85 and kit.servo[14].angle >60:
         carTurnV20()
 
-    elif angle <120 and angle >95:
+    elif kit.servo[14].angle <120 and kit.servo[14].angle >95:
         carTurnO20()
 
-    elif angle <140 and angle >120:
+    elif kit.servo[14].angle <140 and kit.servo[14].angle >120:
         carTurnO40()
 
-    elif angle <160 and angle >140:
+    elif kit.servo[14].angle <160 and kit.servo[14].angle >140:
         carTurnO60()
-        
-   
+
     sleep(1,5)
-    
-    
 
 
 
