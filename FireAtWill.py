c3e1675a18d61d5676b19324ecbba07db367991f
JannePernu <55883062+JannePernu@users.noreply.github.com>
2019-11-01 09:53:20 +0000

    Add files via upload

--- FireAtWill.py
+++ FireAtWill.py
@@ -4,26 +4,13 @@ from time import sleep
 gpio.setwarnings(False)
 
 gpio.setmode(gpio.BOARD)
-gpio.setup(7, gpio.OUT)               # Launch motor pin
+gpio.setup(13, gpio.OUT)               # Launch motor pin
 
 
-def FireAtWill(Debug, dartrdy, DartCount):                       # Shooting function
-        gpio.output(7, True)
-          
-        if Debug == True:    
-            if gpio.input(7):
-                print("port 7 is HIGH")
+def FireAtWill():                       # Shooting function
+        gpio.output(13, True)
                   
-        sleep(5)
-        sol.solenoidburst(Debug)           # After waiting 5 seconds solenoid burst will fire the dart
+        sleep(1)
+        sol.solenoidburst()           # After waiting 5 seconds solenoid burst will fire the dart
 
-        gpio.output(7, False)
-          
-        if Debug == True:    
-            if not gpio.input(7):
-                print("port 7 is LOW")
-                
-        
-        if Debug == True:
-            print("dartrdy is ", dartrdy)
-            print("DartCount is ", DartCount)
+        gpio.output(13, False)
